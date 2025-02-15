cf5bfbf5e5e1196f92fd6203849b23982413177a
khanhbuiduc <ankkhankvjp11@gmail.com>
2025-02-15 23:10:46 +0700

    update

---
+++ olympic sinh vien/2024 learn/bai 1/path.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m, n, k; //<10^-5
+int t;
+int mod = 1e9 + 7;
+int lock[1005][1005];
+int canGo[100005][100005];
+
+int init()
+{
+    cin >> m >> n >> k;
+    cin >> t;
+    int xt, yt;
+}
+int main()
+{
+    freopen("path.inp", "r", stdin);
+    freopen("path.out", "w", stdout);
+}
---
+++ olympic sinh vien/2024/bai4/transf.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> allowed_substrings = {"AA", "BB", "CC", "DDDD", "BABA", "ACAC", "DABCACB"};
+
+struct State {
+    string current;
+    vector<pair<int, int>> steps;
+};
+
+vector<pair<int, int>> get_transformations(string S, string T) {
+    queue<State> q;
+    unordered_set<string> visited;
+    q.push({S, {}});
+    visited.insert(S);
+    
+    while (!q.empty()) {
+        State state = q.front(); q.pop();
+        if (state.current == T) return state.steps;
+        
+        // Try inserting
+        for (int i = 0; i < allowed_substrings.size(); ++i) {
+            const string &sub = allowed_substrings[i];
+            for (int pos = 0; pos <= state.current.size(); ++pos) {
+                string new_str = state.current.substr(0, pos) + sub + state.current.substr(pos);
+                if (visited.find(new_str) == visited.end()) {
+                    visited.insert(new_str);
+                    auto new_steps = state.steps;
+                    new_steps.emplace_back(pos + 1, i + 1);
+                    q.push({new_str, new_steps});
+                }
+            }
+        }
+        
+        // Try deleting
+        for (int i = 0; i < allowed_substrings.size(); ++i) {
+            const string &sub = allowed_substrings[i];
+            size_t pos = 0;
+            while ((pos = state.current.find(sub, pos)) != string::npos) {
+                string new_str = state.current.substr(0, pos) + state.current.substr(pos + sub.size());
+                if (visited.find(new_str) == visited.end()) {
+                    visited.insert(new_str);
+                    auto new_steps = state.steps;
+                    new_steps.emplace_back(pos + 1, -(i + 1));
+                    q.push({new_str, new_steps});
+                }
+                pos += 1;
+            }
+        }
+    }
+    
+    return {}; // No solution found
+}
+
+int main() {
+    freopen("transf.inp", "r", stdin);
+    freopen("transf.out", "w", stdout);
+    string S, T;
+    cin >> S >> T;
+    
+    vector<pair<int, int>> steps = get_transformations(S, T);
+    if (!steps.empty()) {
+        cout << steps.size() << "\n";
+        for (const auto &step : steps) {
+            cout << step.first << " " << step.second << "\n";
+        }
+    } else {
+        cout << -1 << "\n"; // No solution found
+    }
+    
+    return 0;
+}
+
