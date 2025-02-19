fa976c90ddaed193c1c8eb7e0cd91cbcb4193669
Bui Duc Khanh <khanhbuiduc44@gmail.com>
2025-02-19 08:40:16 +0700

    update path

--- olympic sinh vien/2024 learn/bai 1/path.cpp
+++ olympic sinh vien/2024 learn/bai 1/path.cpp
@@ -3,17 +3,39 @@ using namespace std;
 int m, n, k; //<10^-5
 int t;
 int mod = 1e9 + 7;
-int lock[1005][1005];
-int canGo[100005][100005];
-
-int init()
+bool blockGrid[1005][1005];
+int canGo[1005][1005];
+pair<int, int> blockMax; // với xt,yt max
+void dynamic()
+{
+    
+}
+void init()
 {
     cin >> m >> n >> k;
     cin >> t;
-    int xt, yt;
+    int xt, yt, xtmax = 1, ytmax = 11;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> xt >> yt;
+        blockGrid[xt][yt] = true;
+        xtmax = max(xtmax, xt);
+        ytmax = max(ytmax, yt);
+    }
+    blockMax = {xt, yt};
+    for (int i = 0; i <= 1000; i++)
+    {
+        for (int j = 0; j <= 1000; j++)
+        {
+            if (i == xt && j == yt)
+                continue;
+            i = 1;
+        }
+    }
 }
 int main()
 {
     freopen("path.inp", "r", stdin);
     freopen("path.out", "w", stdout);
+    init();
 }
