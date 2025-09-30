0713e85f151be4a1298f7484ea34c994d7c34e36
ados bui <CT060119@actvn.edu.vn>
2025-09-30 19:49:36 +0700

    update algo_course thang 8 buoi1, 2

---
+++ algo_course/Thang8/Buoi1/3. MaximumSubarray/mySol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define FORD(i, b, a) for (int i = (b), _a = (a); i >= _a; i--)
+#define ALL(v) (v).begin(), (v).end()
+int a[100'005];
+int solve(){
+
+}
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        FOR(i,1,n) {cin>>a[i];
+        }
+
+    }
+}
